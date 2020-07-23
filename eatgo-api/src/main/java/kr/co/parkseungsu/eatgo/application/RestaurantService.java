e7d3ac001fa4de196bdad15b3f376304bf350b1e
ParkSeungSu <cldkr0401@naver.com>
2020-07-23 19:19:38 +0900

    study spring boot on FastCampus using IntelliJ

---
+++ eatgo-api/src/main/java/kr/co/parkseungsu/eatgo/application/RestaurantService.java
@@ -0,0 +1,52 @@
+package kr.co.parkseungsu.eatgo.application;
+
+import kr.co.parkseungsu.eatgo.domain.MenuItem;
+import kr.co.parkseungsu.eatgo.domain.MenuItemRepository;
+import kr.co.parkseungsu.eatgo.domain.Restaurant;
+import kr.co.parkseungsu.eatgo.domain.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class RestaurantService {
+    @Autowired
+    RestaurantRepository restaurantRepository;
+
+    @Autowired
+    MenuItemRepository menuItemRepository;
+
+    public RestaurantService(RestaurantRepository restaurantRepository, MenuItemRepository menuItemRepository) {
+        this.restaurantRepository = restaurantRepository;
+        this.menuItemRepository=menuItemRepository;
+    }
+
+    public Restaurant getRestaurant(Long id) {
+
+        Restaurant restaurant = restaurantRepository.findById(id).orElse(null);
+
+        List<MenuItem> menuItems=menuItemRepository.findAllByRestaurantId(id);
+        restaurant.setMenuItems(menuItems);
+
+        return restaurant;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        List<Restaurant> restaurants = restaurantRepository.findAll();
+        return restaurants;
+    }
+
+    public Restaurant addRestaurant(Restaurant restaurant) {
+        return restaurantRepository.save(restaurant);
+
+    }
+
+    @Transactional
+    public Restaurant updateRestaurant(Long id, String name, String address) {
+        Restaurant restaurant=restaurantRepository.findById(id).orElse(null);
+        restaurant.updateInformation(name,address);
+        return restaurant;
+    }
+}
---
+++ eatgo-api/src/test/java/kr/co/parkseungsu/eatgo/interfaces/RestaurantControllerTest.java
@@ -0,0 +1,96 @@
+package kr.co.parkseungsu.eatgo.interfaces;
+
+import kr.co.parkseungsu.eatgo.application.RestaurantService;
+import kr.co.parkseungsu.eatgo.domain.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(RestaurantController.class)
+public class RestaurantControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private RestaurantService restaurantService;
+
+    @Test
+    public void list() throws Exception {
+        List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
+        given(restaurantService.getRestaurants()).willReturn(restaurants);
+        mvc.perform(get("/restaurants"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(
+                        "\"id\":1004")))
+                .andExpect(content().string(containsString(
+                        "\"name\":\"Bob zip\"")));
+    }
+
+    @Test
+    public void detail() throws Exception {
+        Restaurant restaurant1 = new Restaurant(1004L, "Bob zip", "Seoul");
+        restaurant1.addMenuItem(new MenuItem("Kimchi"));
+        Restaurant restaurant2 = new Restaurant(2020L, "Cyber Food", "Seoul");
+        restaurant2.addMenuItem(new MenuItem("Kimchi"));
+        given(restaurantService.getRestaurant(1004L)).willReturn(restaurant1);
+        given(restaurantService.getRestaurant(2020L)).willReturn(restaurant2);
+        mvc.perform(get("/restaurants/1004"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(
+                        "\"id\":1004")))
+                .andExpect(content().string(containsString(
+                        "\"name\":\"Bob zip\"")))
+                .andExpect(content().string((containsString(
+                        "Kimchi"))));
+        mvc.perform(get("/restaurants/2020"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(
+                        "\"id\":2020")))
+                .andExpect(content().string(containsString(
+                        "\"name\":\"Cyber Food\"")));
+    }
+
+    @Test
+    public void create() throws Exception {
+        given(restaurantService.addRestaurant(any())).will(invocation -> {
+            Restaurant restaurant=invocation.getArgument(0);
+            return new Restaurant(1234L,restaurant.getName(),
+                    restaurant.getAddress());
+        });
+
+        mvc.perform(post("/restaurants")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"BeRyong\",\"address\":\"Seoul\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("location","/restaurants/1234"))
+                .andExpect(content().string("{}"));
+
+        verify(restaurantService).addRestaurant(any());
+    }
+
+    @Test
+    public void update() throws Exception{
+        mvc.perform(patch("/restaurants/1004")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content( "{\"name\":\"Bob house\",\"address\":\"Wonju\"}"))
+                .andExpect(status().isOk());
+        verify(restaurantService).updateRestaurant(1004L,"Bob house","Wonju");
+    }
+}
